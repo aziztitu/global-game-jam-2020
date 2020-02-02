c8639158c0ba887a9347245104eac3b52b2a7761
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2020-02-02 15:56:20 -0600

    MainMenuAndCutscene

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class MainMenu : MonoBehaviour {
+
+    public string LevelToLoad;
+    public GameObject mainPanel, creditPanel;
+    public PlayableDirector play;
+
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        creditPanel.gameObject.SetActive(false);
+        mainPanel.gameObject.SetActive(true);
+    }
+
+    public void PlayGame ()
+    {
+        play.Play();
+        StartCoroutine("LoadLevel");
+     
+	}
+
+    public void Back()
+    {
+        creditPanel.gameObject.SetActive(false);
+        mainPanel.gameObject.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        creditPanel.gameObject.SetActive(true);
+        mainPanel.gameObject.SetActive(false);
+    }
+	
+	
+	public void QuitGame()
+    {
+        Application.Quit();
+	}
+
+    public IEnumerator LoadLevel()
+    {
+        yield return new WaitForSeconds(15f);
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
